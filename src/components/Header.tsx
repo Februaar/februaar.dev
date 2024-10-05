fdea128257d9cd138b5ce5b68082d45f7c0715c9
Kyungjin Jung <kyoooungjin@gmail.com>
2024-10-05 23:51:02 +0900

    feat: header 디자인 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,13 @@
 import Link from "next/link";
-import { SiGithub } from "react-icons/si";
-import { FiPaperclip } from "react-icons/fi";
-import { MdMailOutline } from "react-icons/md";
 
-import { HoverCard } from "../components/ui/hovercard";
-
-const GITHUB_LINK = "https://github.com/Februaar";
-const RESUME_LINK = "https://github.com/Februaar";
-
-const propsData = [
+const navigationData = [
   {
-    trigger: () => (
-      <Link href={GITHUB_LINK}>
-        <SiGithub size={24} />
-      </Link>
-    ),
-    content: () => <p className="text-sm">GitHub 페이지로 연결됩니다.</p>,
+    path: "/posts",
+    pathName: "Post",
   },
   {
-    trigger: () => (
-      <Link href={RESUME_LINK}>
-        <FiPaperclip size={24} />
-      </Link>
-    ),
-    content: () => <p className="text-sm">이력서 페이지로 연결됩니다.</p>,
-  },
-  {
-    trigger: () => <MdMailOutline size={24} />,
-    content: () => <p className="text-sm">kyoooungjin@gmail.com</p>,
+    path: "/about",
+    pathName: "About",
   },
 ];
 
@@ -39,13 +19,15 @@ export default function Header() {
       </Link>
 
       <div className="flex space-x-4">
-        <div className="flex gap-2 items-center">
-          {propsData.map((data, index) => (
-            <div key={index}>
-              <HoverCard trigger={data.trigger} content={data.content} />
-            </div>
+        <ul className="flex space-x-4">
+          {navigationData.map((data) => (
+            <li key={data.path} className="text-2xl font-bold">
+              <Link href={data.path}>
+                <button>{data.pathName}</button>
+              </Link>
+            </li>
           ))}
-        </div>
+        </ul>
       </div>
     </header>
   );
--- src/components/Header2.tsx
+++ src/components/Header2.tsx
@@ -1,29 +1,48 @@
 import Link from "next/link";
+import { SiGithub } from "react-icons/si";
+import { FiPaperclip } from "react-icons/fi";
+import { MdMailOutline } from "react-icons/md";
 
-const navigationData = [
+import { HoverCard } from "../components/ui/hovercard";
+
+const GITHUB_LINK = "https://github.com/Februaar";
+const RESUME_LINK = "https://github.com/Februaar";
+
+const propsData = [
   {
-    path: "/posts",
-    pathName: "Post",
+    trigger: () => (
+      <Link href={GITHUB_LINK}>
+        <SiGithub size={24} />
+      </Link>
+    ),
+    content: () => <p className="text-sm">GitHub 페이지로 연결됩니다.</p>,
   },
   {
-    path: "/about",
-    pathName: "About",
+    trigger: () => (
+      <Link href={RESUME_LINK}>
+        <FiPaperclip size={24} />
+      </Link>
+    ),
+    content: () => <p className="text-sm">이력서 페이지로 연결됩니다.</p>,
+  },
+  {
+    trigger: () => <MdMailOutline size={24} />,
+    content: () => <p className="text-sm">kyoooungjin@gmail.com</p>,
   },
 ];
+
 
 export default function Header2() {
   return (
     <section>
       <div className="flex space-x-4">
-        <ul className="flex space-x-4">
-          {navigationData.map((data) => (
-            <li key={data.path} className="text-2xl font-bold">
-              <Link href={data.path}>
-                <button>{data.pathName}</button>
-              </Link>
-            </li>
+        <div className="flex gap-2 items-center">
+          {propsData.map((data, index) => (
+            <div key={index}>
+              <HoverCard trigger={data.trigger} content={data.content} />
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
     </section>
   );
