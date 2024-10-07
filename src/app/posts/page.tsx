dfefe9e0fc49bbf2b623735b64968a08d2130e2a
Kyungjin Jung <kyoooungjin@gmail.com>
2024-10-07 23:52:04 +0900

    design: postdetailpage ui

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,15 @@
+export default function PostDetailPage() {
+  return (
+    <div>
+      <div className="py-8">
+        <h1 className="text-[32px] font-bold mt-6 mb-3">제목</h1>
+        <div>
+          <span>Oct.07.24</span>
+          <div>태그</div>
+        </div>
+      </div>
+
+      <div>내용</div>
+    </div>
+  );
+}
