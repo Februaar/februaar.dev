9cd4ed9237599c8a1b7e17845108e343d2e82204
Kyungjin Jung <kyoooungjin@gmail.com>
2024-10-09 23:28:46 +0900

    design: 메인 배너 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
 import Link from "next/link";
+import Image from "next/image";
 import { FaArrowRight } from "react-icons/fa";
 
-// import Header2 from "../components/Header2";
 import PostCard from "../components/PostCard";
 import Tag from "../components/Tag";
+
+const MAIN_IMAGE = "/icons/main.jpg";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
-      {/* <Header2 /> */}
       {/* <section className="flex flex-col items-center pt-8 pb-6">
         <p>한 가지에 몰두해 내 것으로 만드는 것을 좋아합니다.</p>
       </section> */}
+      <div className="relative px-8 w-full h-72">
+        <Image
+          src={MAIN_IMAGE}
+          alt="main_image"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
 
       <div className="flex flex-wrap items-center justify-center gap-2 my-16 mx-20 space-x-2">
         <Tag />
