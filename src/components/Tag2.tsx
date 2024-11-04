49af81bda5274fd1541d78b4032ecedba5817c0a
Kyungjin Jung <kyoooungjin@gmail.com>
2024-11-04 23:16:51 +0900

    fix: 타입 에러 수정

--- src/components/Tag2.tsx
+++ src/components/Tag2.tsx
@@ -1,4 +1,8 @@
-export default function Tag2({ tags }: any) {
+interface Tag {
+  tags: string[];
+}
+
+export default function Tag2({ tags }: Tag) {
   return (
     <div className="w-fit mb-4">
       <span className="bg-gray-primary rounded-[8px] text-black text-[14px] py-1 px-2">
