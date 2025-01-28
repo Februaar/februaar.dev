31de150baddaf23a5c86213ad6657ad47ba4e2d6
Kyungjin Jung <kyoooungjin@gmail.com>
2025-01-29 01:54:31 +0900

    update new contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,20 @@ export default async function Home() {
   return (
     <article>
       <div className="mt-12 mb-8">
+        <span className="text-h3 font-bold ">TIL</span>
+      </div>
+      <ul className="post-list">
+        {pinnedPosts.map((post) => (
+          <PostCard key={post.slug} posts={post} />
+        ))}
+      </ul>
+
+      <div className="mt-12 mb-8">
         <span className="text-h3 font-bold ">Recent Posts</span>
       </div>
       <ul className="post-list">
         {sortedPosts.slice(0, 5).map((sortedPost) => (
           <PostCard key={sortedPost.slug} posts={sortedPost} />
-        ))}
-      </ul>
-
-      <div className="mt-12 mb-8">
-        <span className="text-h3 font-bold ">Learn</span>
-      </div>
-      <ul className="post-list">
-        {pinnedPosts.map((post) => (
-          <PostCard key={post.slug} posts={post} />
         ))}
       </ul>
 
