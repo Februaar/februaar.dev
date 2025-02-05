7132d9b95001665ae042af7ea070eedc2c151241
Kyungjin Jung <kyoooungjin@gmail.com>
2025-02-05 23:42:37 +0900

    new contents

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="mx-auto px-4 mb-8">
-      <p className="text-sm font-light">© 2024 februaar.</p>
+      <p className="text-sm font-light">© 2025 februaar.</p>
     </footer>
   );
 }
